31a8d97711bb48c42bfe6266d27e0edd92cfffea
rosa-games-01 <gavinisbestandgreat@gmail.com>
2026-04-24 09:24:28 -0400

    Remove makecloak function
    Removed the makecloak function that generates a clone of the current window.

--- js/index.js
+++ js/index.js
@@ -432,28 +432,6 @@ function toggleStar(event, star) {
    *
    * @return {void}
    */
-  function makecloak(replaceUrl = preferences.cloakUrl) {
-      if (window.top.location.href !== 'about:blank') {
-          var url = window.location.href;
-          const win = window.open();
-          if (!win || win.closed || typeof win.closed == 'undefined') {
-              return;
-          }
-          win.document.body.style.margin = '0';
-          win.document.body.style.height = '100vh';
-          var iframe = win.document.createElement('iframe');
-          iframe.style.border = 'none';
-          iframe.style.width = '100%';
-          iframe.style.height = '100%';
-          iframe.style.margin = '0';
-          iframe.referrerpolicy = 'no-referrer';
-          iframe.allow = 'fullscreen';
-          iframe.src = url.toString();
-          win.document.body.appendChild(iframe);
-          window.location.replace(replaceUrl);
-      }
-  }
-
   /**
    * Changes the browser tab's title and favicon
    *
